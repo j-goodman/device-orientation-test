af786f2bd5212d3b77f122756d8b988a4b6b3c49
John Goodman <john.goodman@joystickinteractive.com>
2017-03-23 18:09:24 -0400

    Display results of orientation event

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function handleOrientation(event) {
   var beta     = event.beta;
   var gamma    = event.gamma;
 
-  square.innerText = "Haldo.";
+  square.innerText = event.absolute + "-" + event.alpha + "-" + event.beta + "-" + event.gamma;
 }
 
 onkeydown = handleOrientation;
